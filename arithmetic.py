1224e2ba9b6df3d3ffd0c3bbad0ab0ee17942eb8
SanekLord <106690518+SanekLord@users.noreply.github.com>
2022-07-29 14:09:12 +0300

    job1

---
+++ arithmetic.py
@@ -0,0 +1,5 @@
+# пример решить и выводит твой ответ и правильный
+cond = input("Solve this example: 4 * 100 - 54=")
+ans = 4 * 100 - 54
+print(f"Your answer:{cond}")
+print(f"Correct answer:{ans}")
---
+++ user.py
@@ -0,0 +1,10 @@
+# ваше имя, возрост, город
+u_name = input("What is your name?")
+u_old = input("How old are you?")
+u_city = input("Where are you live?")
+print("Your name", u_name)
+print("Your age", u_old)
+print("Your city", u_city)
+
+
+
